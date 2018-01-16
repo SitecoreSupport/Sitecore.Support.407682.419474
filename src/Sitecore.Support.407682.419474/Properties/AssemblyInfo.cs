c56d595b42b8124a28500708b9a9449cd31eaba8
Alexey Tupkalo <altu@sitecore.net>
2018-01-16 14:29:04 +0200

    Patch 407682.419474 initial commit

---
+++ src/Sitecore.Support.407682.419474/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.407682.419474")]
+[assembly: AssemblyProduct("Sitecore.Support.407682.419474")]
+[assembly: ComVisible(false)]
